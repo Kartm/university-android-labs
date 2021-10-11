120408d89b86fc0cff850c123a4ce47db73667e3
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-10-11 19:29:49 +0200

    Add custom themes for activities

--- app1/app/src/main/java/dma/pwr/app1/MainActivity2.kt
+++ app1/app/src/main/java/dma/pwr/app1/MainActivity2.kt
@@ -1,18 +1,19 @@
 package dma.pwr.app1
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity2 : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         val window2 = findViewById<View>(R.id.window2)
         window2.setOnLongClickListener(View.OnLongClickListener {
-            finish();
-            return@OnLongClickListener false;
+            finish()
+            return@OnLongClickListener false
         })
     }
 }
--- app1/app/src/main/java/dma/pwr/app1/MainActivity3.kt
+++ app1/app/src/main/java/dma/pwr/app1/MainActivity3.kt
@@ -1,22 +1,23 @@
 package dma.pwr.app1
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 
-class MainActivity3 : AppCompatActivity() {
+class MainActivity3 : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
     }
 
-    fun goBackTo1(view: View) {
-        onBackPressed();
+    fun goBackTo1() {
+        onBackPressed()
     }
 
     override fun onStop() {
-        super.onStop();
+        super.onStop()
 
         Toast.makeText(applicationContext, "Activity 3 - stopped", Toast.LENGTH_SHORT).show()
     }
