90523b6ee7e06e23a8d5d29093c5b0dce468b8a6
Lukasz Blachnicki <lblachnicki@gmail.com>
2021-11-16 20:22:38 +0100

    Color boxes after clicking buttons

--- ColorMyViews/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -11,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var box_three_text: View;
     private lateinit var box_four_text: View;
     private lateinit var box_five_text: View;
+    private lateinit var red_button: View;
+    private lateinit var green_button: View;
+    private lateinit var blue_button: View;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +24,16 @@ class MainActivity : AppCompatActivity() {
         box_three_text = findViewById<View>(R.id.box_three_text)
         box_four_text = findViewById<View>(R.id.box_four_text)
         box_five_text = findViewById<View>(R.id.box_five_text)
+        red_button = findViewById<View>(R.id.red_button)
+        green_button = findViewById<View>(R.id.green_button)
+        blue_button = findViewById<View>(R.id.blue_button)
 
         setListeners()
     }
 
     private fun setListeners() {
-        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text)
+        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text,
+            box_four_text, box_five_text, red_button, green_button, blue_button)
 
         for(item in clickableViews) {
             item.setOnClickListener {makeColored(it)}
@@ -44,6 +51,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.blue_button -> box_four_text.setBackgroundResource(R.color.my_blue)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
